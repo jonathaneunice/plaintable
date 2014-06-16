1d7350c8bb6acd803008066a41b32ddd97f04876
Stefan Tatschner <stefan@sevenbyte.org>
2014-06-16 17:57:27 +0200

    Typos

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('README.rst') as f:
 setup(
     name='plaintable',
     version=plaintable.__version__,
-    description='a simple library to build plain text tables',
+    description='A simple library to build plain text tables',
     long_description=long_description,
     url='https://github.com/rumpelsepp/plaintable',
     author='Stefan Tatschner',
     author_email='stefan@sevenbyte.org',
     license=plaintable.__license__,
-    py_modules = ['plaintable'],
+    py_modules=['plaintable'],
 
     classifiers=[
         'Development Status :: 3 - Alpha',
