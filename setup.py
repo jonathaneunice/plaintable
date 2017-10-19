08ce60e28cd356a5455919bcd3bea485d105a39f
Jonathan Eunice <jonathan.eunice@gmail.com>
2017-10-18 22:25:18 -0400

    extend setup.py to reflect wider Python version support

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    keywords='table development'
+    keywords='table development format tabular'
 )
